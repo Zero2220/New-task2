f0d8b85b01b814f522bf8bea5c3d15906f6da473
anarpi <anarpi@code.edu.az>
2024-01-23 16:22:45 +0400

    new

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fullName;
+            do
+            {
+                Console.WriteLine("adi ve soyadi daxil edin ");
+                fullName = Console.ReadLine();
+
+            }
+            while (!checkFullName(fullName));
+
+            string GroupName;
+            do
+            {
+                Console.WriteLine("group nomresini daxil edin ");
+                GroupName = Console.ReadLine();
+
+            }
+            while (!checkGroupName(GroupName));
+            int age;
+            string agestr;
+            do
+            {
+                Console.WriteLine("yasi daxil edin ");
+                agestr = Console.ReadLine();
+
+            }
+            while (!int.TryParse(agestr,out age));
+
+            Student student = new Student(fullName,GroupName,age);
+            
+
+            Console.WriteLine(student.Fullname);
+            Console.WriteLine(student.GroupNo);
+            Console.WriteLine(student.Age);
+
+
+
+        }
+
+
+        static bool checkFullName(string fullName)
+        {
+            bool result;
+            fullName.Trim();
+
+            int count = fullName.Length - fullName.Replace(" ", "").Length;
+
+            if (fullName != null && count == 1 && fullName.Length >= 8)
+            {
+                result = true;
+
+
+            }
+            else result = false;
+
+            return result;
+
+
+        }
+
+        static bool checkGroupName(string groupName)
+        {
+
+            groupName.Trim();
+            
+            string  groupName1 = groupName.ToLower();
+            string  groupName2 = groupName1.ToUpper();
+
+
+            if (groupName != null && groupName.Length == 4 && char.IsLetter(groupName1, 0) && char.IsLetter(groupName2, 0))
+            {
+                return true;
+            }
+            else return false;
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Student
+    {
+        public Student(string Fullname,string GroupNo,int Age) 
+        {
+          this.Fullname = Fullname;
+          this.GroupNo = GroupNo;
+          this.Age = Age;
+        }
+
+
+
+        public string Fullname;
+        public string GroupNo;
+        public int Age;
+    }
+}
